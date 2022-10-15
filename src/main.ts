d5ce1f4551f8c0167f42e614af4fc47f5c2a7674
Patrick <patrickk0806@hotmail.com>
2022-10-15 19:51:28 -0300

    feat: add api dinymic port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
